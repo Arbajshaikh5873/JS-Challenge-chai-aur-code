ed6159ec23ef3c9dd3d02f8a37fe9ab690a185cb
Arbajshaikh5873 <155817228+Arbajshaikh5873@users.noreply.github.com>
2024-07-13 08:03:21 +0000

    day-1

---
+++ Day-1/task.js
@@ -0,0 +1,55 @@
+// task-1
+
+var num;
+num=100;
+console.log(`value of number  is ${num}`);
+
+// task-2
+
+let s;
+s="Hello World !!";
+console.log(`The String is ${s}`);
+
+// task-3
+
+const bool= false;
+console.log(`Boolean value is ${bool}`);
+
+// task-4
+
+let  n = 20;
+console.log("type of "+num+" is "+ typeof n );
+
+let  str ="Hello World!!";
+console.log("type of "+str+" is "+ typeof str);
+
+let  boolean = true;
+console.log("type of "+boolean+" is "+ typeof boolean );
+
+let array= ["a","e","i","o","u"];
+console.log("type of "+array+" is "+ typeof array );
+/*
+note:-
+      type of array is object
+*/
+
+let obj = {
+    name: "Arbaj J Shaikh",
+    age: 21
+}
+console.log("type of object is " + typeof obj );
+
+// task-5
+
+let n1=10;
+console.log(n1);
+let n1=20;
+console.log(n1);
+
+// task-6
+
+const c1=10;
+console.log(n1);
+const c1=20;
+console.log(n1);
+// this gives error
