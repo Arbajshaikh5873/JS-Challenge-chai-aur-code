37764e73a0eea4880b6c80633e38b5b35cd52ccb
Arbajshaikh5873 <155817228+Arbajshaikh5873@users.noreply.github.com>
2024-07-18 16:01:17 +0000

    day-6

---
+++ Day-6/task.js
@@ -0,0 +1,18 @@
+
+//Activity 1
+
+let a= [1,2,3,4,5];
+console.log(a);
+console.log("Frist Element is "+a[0]);
+console.log("Last Element is "+a[a.length-1]);
+
+//Activity 2
+
+
+console.log("added  Element at position "+a.push(10));
+
+console.log("Removing Element "+ a.pop());
+console.log("Removing Element "+ a.shift());
+console.log( a);
+console.log("Removing Element "+ a.unshift(1));
+console.log( a);
